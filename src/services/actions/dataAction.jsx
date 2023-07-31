04f26533f838c159df2910b6bd95b1443f02403c
farhan0184 <shahariar0184@gmail.com>
2023-07-31 21:40:31 +0600

    last commit

--- src/services/actions/dataAction.jsx
+++ src/services/actions/dataAction.jsx
@@ -5,7 +5,7 @@ import { GET_DATA_FAILED, GET_DATA_REQUEST, GET_DATA_SUCCESS } from "../constant
 export const getAllData = () => async (dispatch)=>{
     dispatch({type: GET_DATA_REQUEST});
     try {
-        const res = await axios.get("http://web-api-tht-env.eba-kcaa52ff.us-east-1.elasticbeanstalk.com/api/dev/products");
+        const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
         dispatch({type: GET_DATA_SUCCESS, payload: res.data});
     } catch (error) {
         dispatch({type: GET_DATA_FAILED, payload: error.message})
